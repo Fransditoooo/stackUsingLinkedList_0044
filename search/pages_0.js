94550c7a77fcb0cd7863c73f0ca556b4a1afe797
Fransditoooo <fransdito.bayu.ft24@mail.umy.ac.id>
2025-07-06 20:17:51 +0700

    documentation c++ program

---
+++ search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['dokumen_20project_20stack_20linked_20list_0',['Dokumen Project Stack Linked List',['../index.html',1,'']]]
+];
--- stack.cpp
+++ stack.cpp
@@ -1,13 +1,56 @@
+/**
+ * @file stack.cpp
+ * @mainpage Dokumen Project Stack Linked List
+ * 
+ * @section Deskripsi
+ * Program ini merupakan implementasi struktur data stack (tumpukan).
+ * Mendukung operasi dasar seperti:
+ * - push
+ * - pop
+ * - display
+ * 
+ * @section Cara Penggunaan
+ * Jalankan program dan pilih menu:
+ * 1. push 
+ * 2. pop
+ * 3. display
+ * 4. exit
+ * 
+ * @section Penulis
+ * - Nama: Fransdito Bayu Pratama
+ * - Nim : 20240140044
+ * - Kelas : A
+ * - Tanggal : 23 Juni 2025
+ */
+
+/**
+ * @file stack.cpp
+ * @author Fransdito 
+ * @brief ini adalahh program stack menggunakan linked list
+ * @version 0.1
+ * @date 2025-06-23
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
 #include <iostream>
 using namespace std;
 
 // Node class representing a singel node in the linked list
+/**
+ * @class Node
+ * @brief Class Node untuk mneyimpan data dan alamat memori
+ */
 class Node
 {
 public:
-int data;
-Node *next;
+int data; ///< variabel data dengan tipe intergen
+Node *next; ///< variabel untuk menyimpan alamat memori
 
+/**
+ * @brief Construct a new Node object
+ * 
+ */
 Node()
 {
     next = NULL;
@@ -25,6 +68,15 @@ Stack()
 {
     top = NULL; //Initlalize the stack with a null top pointer
 }
+
+public:
+    //Push operation Insert an Element onto the top of the stack
+    /**
+     * @brief Method push 
+     * 
+     * @param value menerima input 
+     * @return int
+     */
 
 //Push operation: Insert an elemen onto the top of the stack
 int push(int value)
@@ -73,12 +125,22 @@ void pop()
     }
 };
 
+/**
+ * @brief ini adalah function main yang berfungsi menjalankan program di terminal
+ * 
+ * @return int 
+ */
 int main()
 {
     Stack stack;
     
-    int choice = 0;
-    int value;
+    int choice = 0; ///< variabel yang akan digunakan untuk menyimpan pilihan pada operasi switch case 
+    int value;      ///< variabel yang akan digunakan untuk  memasukkan nilai data kedalam stack
+
+    /**
+     * @brief ini adalah operasi witch case yang akan digunakan untuk push, pop, dan display data yang dimasukkan
+     * 
+     */
 
     while (choice !=4)
     {
@@ -93,13 +155,13 @@ int main()
             case 1:
             cout << "enter the value to push:";
             cin >> value;
-            stack.push(value);
+            stack.push(value); ///< push value yang dimasukkan ke stack
             break;
             case 2:
-            stack.pop();
+            stack.pop(); ///< mengeluarkan topmost di elemen dari stack
             break;
             case 3:
-            stack.peek();
+            stack.peek(); ///< untuk melihat isi data di list
             break;
 
             case 4:
